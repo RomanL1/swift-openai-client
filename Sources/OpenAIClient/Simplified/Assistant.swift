09783acf7621f21fa21052f5ecd725539690d1bc
Nicolas Märki <maerki@users.noreply.github.com>
2024-07-01 20:38:01 +0200

    add function call api

--- Sources/OpenAIClient/Simplified/Assistant.swift
+++ Sources/OpenAIClient/Simplified/Assistant.swift
@@ -1,11 +1,12 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Nicolas Märki on 27.06.2024.
 //
 
 import Foundation
+import OpenAPIRuntime
 
 public struct Assistant {
 
@@ -40,21 +41,51 @@ public struct Assistant {
             return Message(raw: raw)
         }
 
-        public func run() async throws -> [Message] {
-            let run = try await client.createRun(path: .init(thread_id: id), body: .json(.init(assistant_id: assistant.id, stream: true))).ok.body.text_event_hyphen_stream
+        public struct CallArgument {
+            let input: String
+            public func decoded<T: Decodable>(as: T.Type = T.self) throws -> T {
+                return try JSONDecoder().decode(T.self, from: input.data(using: .utf8)!)
+            }
+        }
+
+        public func run(functions: [String: ((CallArgument) throws -> Encodable)] = [:]) async throws -> [Message] {
+            var runStream: OpenAPIRuntime.HTTPBody? = try await client.createRun(path: .init(thread_id: id), body: .json(.init(assistant_id: assistant.id, stream: true))).ok.body.text_event_hyphen_stream
 
             var messages: [Components.Schemas.MessageObject] = []
+            var calls: [String] = []
 
-            for try await event in run.asDecodedServerSentEvents() {
-                if event.data == "[DONE]" { break }
-                if let data = event.data?.data(using: .utf8) {
-                    let eventData = try JSONDecoder().decode(Components.Schemas.RunStreamEvent.self, from: data)
-                    switch eventData {
-                        case .MessageObject(let message):
-                            messages.removeAll { $0.id == message.id }
-                            messages.append(message)
-                            break;
-                        default: break
+            while let lastStream = runStream {
+                runStream = nil
+                for try await event in lastStream.asDecodedServerSentEvents() {
+                    if event.data == "[DONE]" { break }
+                    if let data = event.data?.data(using: .utf8) {
+                        let eventData = try JSONDecoder().decode(Components.Schemas.RunStreamEvent.self, from: data)
+                        switch eventData {
+                            case .MessageObject(let message):
+                                messages.removeAll { $0.id == message.id }
+                                messages.append(message)
+                                break;
+                            case .RunObject(let run):
+                                if let requiredAction = run.required_action {
+                                    var outputs: [Components.Schemas.SubmitToolOutputsRunRequest.tool_outputsPayloadPayload] = []
+                                    for call in requiredAction.submit_tool_outputs.tool_calls {
+                                        if calls.contains(call.id) {
+                                            continue
+                                        }
+                                        calls.append(call.id)
+                                        if let f = functions[call.function.name] {
+                                            let response = try f(CallArgument(input: call.function.arguments))
+                                            let encoded = String(data: try JSONEncoder().encode(response), encoding: .utf8)!
+                                            outputs.append(.init(tool_call_id: call.id, output: encoded))
+                                        }
+                                        else {
+                                            throw OpenAIError(errorDescription: "Tool \(call.function.name) not a known function")
+                                        }
+                                    }
+                                    runStream = try await client.submitToolOutputsToRun(path: .init(thread_id: id, run_id: run.id), body: .json(.init(tool_outputs: outputs, stream: true))).ok.body.text_event_hyphen_stream
+                                }
+                            default: break
+                        }
                     }
 
                 }
@@ -67,7 +98,10 @@ public struct Assistant {
 
 
 
+
+
 }
+
 
 
 
